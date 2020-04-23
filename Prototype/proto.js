41e79747f786de68f15de617b4130aa7e50b1432
Therealajahn <therealajahn@gmail.com>
2020-04-22 19:54:46 -0500

    replaced code in design doc with better formatting; prototyped the explanation section of the doc

---
+++ Prototype/proto.js
@@ -0,0 +1,6 @@
+let array = [1,2,3,4];
+console.log('index zero', array[0]); // 1
+console.log('index one', array[1]); // 2
+console.log('index two', array[2]); // 3
+console.log('index three', array[3]); // 4
+

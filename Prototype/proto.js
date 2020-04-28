55cc36e9e2b25d33cb6e2bc31784aba145a234be
Therealajahn <therealajahn@gmail.com>
2020-04-28 05:11:51 -0500

    update

--- Prototype/proto.js
+++ Prototype/proto.js
@@ -1,6 +1,4 @@
-let array = [1,2,3,4];
-console.log('index zero', array[0]); // 1
-console.log('index one', array[1]); // 2
-console.log('index two', array[2]); // 3
-console.log('index three', array[3]); // 4
+for (let i = 0; i < 16; i++){
+    console.log(i);
+}
 

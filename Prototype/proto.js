3eed931d1e6effa9a75d5e656879ec984a9d6560
Therealajahn <therealajahn@gmail.com>
2020-04-28 06:15:37 -0500

    using nested loops to render

--- Prototype/proto.js
+++ Prototype/proto.js
@@ -1,4 +1,16 @@
+let chart = document.getElementById('chart-container');
+
+let num = ['one', 'two', 'three', 'four','five', 'six','seven', 'eight','nine','ten','eleven', 'twelve','thirteen','fourteen','fifteen','sixteen'];
+
+let section = [];
+
 for (let i = 0; i < 16; i++){
-    console.log(i);
+    section.push(`<section class = "big-boy ${num[i]}">`);
+        for (let j = 0; j < 16; j++) {
+           section.push(`<div class='small-boy ${num[j]}'></div>`)
+        } 
+    section.push('</section>');   
 }
+
+chart.innerHTML = section.join(' ');
 

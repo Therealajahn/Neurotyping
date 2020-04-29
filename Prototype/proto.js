870fcda6d98e24e98ddc491ec09aa19055047d91
Therealajahn <therealajahn@gmail.com>
2020-04-28 20:03:32 -0500

    used a for loop to make chart, is a bit convoluted

--- Prototype/proto.js
+++ Prototype/proto.js
@@ -2,14 +2,25 @@ let chart = document.getElementById('chart-container');
 
 let num = ['one', 'two', 'three', 'four','five', 'six','seven', 'eight','nine','ten','eleven', 'twelve','thirteen','fourteen','fifteen','sixteen'];
 
+let colorNum = -1;
 let section = [];
-
-for (let i = 0; i < 16; i++){
-    section.push(`<section class = "big-boy ${num[i]}">`);
-        for (let j = 0; j < 16; j++) {
-           section.push(`<div class='small-boy ${num[j]}'></div>`)
-        } 
-    section.push('</section>');   
+for (let i = 0; i < 256; i++){
+    let sixteenDiv = i % 16;
+    // console.log("sixteenDiv:",sixteenDiv);
+    
+    if(sixteenDiv === 0){ 
+     colorNum++;
+        // console.log( colorNum" colorNum);
+        section.push(`<section class = "big-boy ${num[colorNum]}">`);
+    }    
+    console.log('colorNum:', colorNum);
+    section.push(`<div class='small-boy ${num[colorNum]}'></div>`)
+    
+    if(sixteenDiv === 15){   
+    section.push('</section>'); 
+    };
+    
+    // console.log(section);  
 }
 
 chart.innerHTML = section.join(' ');

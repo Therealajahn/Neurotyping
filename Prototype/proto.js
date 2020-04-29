c843c33472ccbbb3ef4db47216772d9f16917103
Therealajahn <therealajahn@gmail.com>
2020-04-29 07:17:40 -0500

    added terrible looking menu bar, will remove post-haste

--- Prototype/proto.js
+++ Prototype/proto.js
@@ -6,21 +6,26 @@ let color = ['one', 'two', 'three', 'four','five', 'six','seven', 'eight','nine'
 
 let chart = '';
 
-for (let i = 0; i < 256; i++){
-    
-    let sixteenDiv = i % 16;
-    
-    if(sixteenDiv === 0){
-        colorIndex++;
-        console.log("colorIndex",colorIndex);
-        chart += `\n\n<section class = 'big-boy ${color[colorIndex]}'>\n`;
-    }; 
-    
-    chart += `\n\t\t<div class='small-boy ${color[colorIndex]}'></div>`;
+    for (let i = 0; i < 256; i++){
+        
+        let sixteenDiv = i % 16;
+        
+        if(sixteenDiv === 0){
+            colorIndex++;
+            console.log("colorIndex",colorIndex);
+            chart += `\n\n<section class = 'big-boy ${color[colorIndex]}'>\n`;
+        }; 
+        
+        chart += `\n\t\t<div class='small-boy ${color[colorIndex]}'></div>`;
 
-    if(sixteenDiv === 15){
-        chart += '</section>';
+        if(sixteenDiv === 15){
+            chart += '</section>';
+        }
     }
-}
 console.log("chart",chart);
+
 chartContainer.innerHTML = chart;
+
+
+
+
